a190938e1a7ff08f61b32b4974316ec41ce14094
OxygenO22 <bamaik@mail.ru>
2024-09-27 17:34:46 +0300

    first commit

---
+++ src/common/api/common.api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  withCredentials: true,
+  headers: {
+    "API-KEY": "c658e6f4-b2ed-4db1-aa3f-e647bb94e78c",
+  },
+});
